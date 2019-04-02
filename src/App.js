a4c2a71f8482ca4c6bc2297377d7e53ed94c998b
Ahmed Hadjou <thehidjou@yahoo.com>
2019-04-02 15:01:09 +0100

    Part 34

--- src/App.js
+++ src/App.js
@@ -23,6 +23,9 @@ import axios from 'axios';
 
 const theme = createMuiTheme(themeObject);
 
+axios.defaults.baseURL =
+  'https://europe-west1-socialape-d081e.cloudfunctions.net/api';
+
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
