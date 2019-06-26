030c99ca4168c8e842d0c3ccf7b9fe47ed9e3ac9
Ahmed Hadjou <thehidjou@yahoo.com>
2019-06-26 08:48:39 +0100

    checking if redux devtools ext is installed before starting it

--- src/redux/store.js
+++ src/redux/store.js
@@ -15,13 +15,12 @@ const reducers = combineReducers({
   UI: uiReducer
 });
 
-const store = createStore(
-  reducers,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
+const store = createStore(reducers, initialState, enhancer);
 
 export default store;
